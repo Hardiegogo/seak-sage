1ee0a0bb40e0bcec9efdc6475c5057141d875115
Chirag Gupta <chirag.gps@gmail.com>
2023-07-06 00:08:52 +0530

    fix

--- packages/backend/src/config/database.ts
+++ packages/backend/src/config/database.ts
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 import { config } from 'dotenv';
 import path from 'path';
 config({ path: path.resolve(__dirname, '../../.env') });
-mongoose.connect(process.env.MONGOOSE_SECRET);
+mongoose.connect(process.env.MONGOOSE_SECRET as string);
